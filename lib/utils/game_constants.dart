3117faa8059851929a1a63430b40963bbc4f0361
yugalpatil23 <yugalpatil23@gmail.com>
2026-05-08 18:26:33 +0530

    added released

--- lib/utils/game_constants.dart
+++ lib/utils/game_constants.dart
@@ -1,5 +1,5 @@
 class GameConstants {
-  static String developerEmail = " alphay28071994@gmail.com";
+  static String developerEmail = "alphay28071994@gmail.com";
   static String privacyPolicyUrl = "https://yourwebsite.com/privacy";
   static String termsOfServiceUrl = "https://yourwebsite.com/terms";
 }
